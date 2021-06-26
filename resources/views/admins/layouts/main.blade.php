aea10ac23930a7315f6a975a62007d9f51ea2e15
fhmanwar <fhmianwar@gmail.com>
2021-06-26 10:10:34 +0700

    update secure asset

--- resources/views/admins/layouts/main.blade.php
+++ resources/views/admins/layouts/main.blade.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Laporan Data Penjualan Makanan</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-    <link rel="icon" href="{{ asset('/template/assets/img/icon.ico') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ secure_asset('/template/assets/img/icon.ico') }}" type="image/x-icon" />
 
     <!-- Fonts and icons -->
-    <script src="{{ asset('/template/assets/js/plugin/webfont/webfont.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/plugin/webfont/webfont.min.js') }}"></script>
 
     <script>
         WebFont.load({
@@ -29,9 +29,9 @@
     </script>
 
     <!-- CSS Files -->
-    {{-- <link rel="stylesheet" href="{{ asset('/css/plugin/jquery.dataTables.min.css') }}"> --}}
-    <link rel="stylesheet" href="{{ asset('/template/assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/template/assets/css/azzara.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ secure_asset('/css/plugin/jquery.dataTables.min.css') }}"> --}}
+    <link rel="stylesheet" href="{{ secure_asset('/template/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/template/assets/css/azzara.min.css') }}">
     @yield('style')
 </head>
 
@@ -61,53 +61,53 @@
         </div>
     </div>
     <!--   Core JS Files   -->
-    <script src="{{ asset('/js/plugin/jquery-3.5.1.min.js') }}"></script>
-    <script src="{{ asset('/template/assets/js/core/popper.min.js') }}"></script>
-    <script src="{{ asset('/template/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/core/bootstrap.min.js') }}"></script>
 
     <!-- jQuery UI -->
-    <script src="{{ asset('/template/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
-    <script src="{{ asset('/template/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js') }}"></script>
 
     <!-- jQuery Scrollbar -->
-    <script src="{{ asset('/js/plugin/jquery.scrollbar.min.js') }}"></script>
-    <script src="{{ asset('/js/plugin/jquery.ui.touch-punch.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/jquery.scrollbar.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/jquery.ui.touch-punch.min.js') }}"></script>
 
     <!-- Moment JS -->
-    <script src="{{ asset('/template/assets/js/plugin/moment/moment.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/plugin/moment/moment.min.js') }}"></script>
 
     <!-- Chart JS -->
-    <script src="{{ asset('/template/assets/js/plugin/chart.js/chart.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/plugin/chart.js/chart.min.js') }}"></script>
 
     <!-- jQuery Sparkline -->
-    <script src="{{ asset('/template/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
 
     <!-- Datatables -->
-    <script src="{{ asset('/template/assets/js/plugin/datatables/datatables.min.js') }}"></script>
-    <script src="{{ asset('/js/plugin/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('/js/plugin/dataTables.editor.min.js') }}"></script>
-    <script src="{{ asset('/js/plugin/buttons.flash.min.js') }}"></script>
-    <script src="{{ asset('/js/plugin/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('/js/plugin/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('/js/plugin/jszip.min.js') }}"></script>
-    <script src="{{ asset('/js/plugin/pdfmake.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/plugin/datatables/datatables.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/dataTables.editor.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/buttons.flash.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/buttons.html5.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/buttons.print.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/jszip.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/pdfmake.min.js') }}"></script>
 
     <!-- Bootstrap Notify -->
-    <script src="{{ asset('/js/plugin/bootstrap-notify.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/bootstrap-notify.min.js') }}"></script>
 
     <!-- Bootstrap Toggle -->
-    <script src="{{ asset('/js/plugin/bootstrap-toggle.min.js') }}"></script>
+    <script src="{{ secure_asset('/js/plugin/bootstrap-toggle.min.js') }}"></script>
 
     <!-- Sweet Alert -->
-    <script src="{{ url('js/plugin/sweetalert2@9.js') }}"></script>
+    <script src="{{ secure_asset('js/plugin/sweetalert2@9.js') }}"></script>
 
     <!-- Chart amChart -->
-    <script src="{{ asset('js/plugin/amcharts4/core.js') }}"></script>
-    <script src="{{ asset('js/plugin/amcharts4/charts.js') }}"></script>
-    <script src="{{ asset('js/plugin/amcharts4/themes/animated.js') }}"></script>
+    <script src="{{ secure_asset('js/plugin/amcharts4/core.js') }}"></script>
+    <script src="{{ secure_asset('js/plugin/amcharts4/charts.js') }}"></script>
+    <script src="{{ secure_asset('js/plugin/amcharts4/themes/animated.js') }}"></script>
 
     <!-- Azzara JS -->
-    <script src="{{ asset('/template/assets/js/ready.min.js') }}"></script>
+    <script src="{{ secure_asset('/template/assets/js/ready.min.js') }}"></script>
     @yield('script')
 </body>
 
